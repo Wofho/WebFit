bd3147b7ae6e711e9e749fa6985b6762c1bdeb24
Wofho <ltemelakiev@gmail.com>
2014-04-15 21:56:08 +0300

    Revert "Revert "retrieveforgottenpass""
    This reverts commit 6da3d4f38ad1787a9ecda6058098e442a0f80e33.

--- site/8.php
+++ site/8.php
@@ -60,6 +60,10 @@
 		#wb_element_instance135 li a { text-transform: none; border: 0px none #000000; padding: 10px 50px 10px 50px; font: normal bold 18px Arial,Helvetica,sans-serif; text-decoration: none; color: #ffffff; line-height: 20px; background: transparent none repeat scroll left top; }
 		#wb_element_instance135 li:hover > a, #wb_element_instance135 li:focus > a { border: 0px none #000000; font: normal bold 18px Arial,Helvetica,sans-serif; text-decoration: none; color: #363636; line-height: 20px; background: transparent none no-repeat scroll center bottom; }
 		#wb_element_instance135 li.active > a { border: 0px none #000000; font: normal bold 18px Arial,Helvetica,sans-serif; text-decoration: none; color: #363636; line-height: 20px; background: transparent none no-repeat scroll center bottom; }
+		#wb_element_instance139 a { width: 100%; height: 100%; font-family: Helvetica,Arial,sans-serif; font-size: 14px; text-align: center; border: 1px solid #cccccc; border-radius: 6px; -webkit-border-radius: 6px; -moz-border-radius: 6px; color: #333333; font-style: normal; font-weight: normal; text-decoration: none; background: #f5f5f5 none repeat scroll left top; }
+		#wb_element_instance139 a:hover { color: #333333; font-style: normal; font-weight: normal; text-decoration: none; background: #E6E6E6 none repeat scroll left top; }
+		#wb_element_instance139 a:active { color: #333333; font-style: normal; font-weight: normal; text-decoration: none; background: #E6E6E6 none repeat scroll left top; }
+		#wb_element_instance139 a span { padding: 4px 0px 0px 0px; }
 	</style>
 	<link href="css/site.css?v=1.0.3" rel="stylesheet" type="text/css" />
 		
@@ -70,13 +74,13 @@
 </head>
 
 
-<body>{{ga_code}}<div class="root" style="width: 1005px; height: 590px;"><div class="vbox wb_container" id="wb_header" style="height: 150px; background: transparent none repeat scroll left top;">
+<body>{{ga_code}}<div class="root" style="width: 1005px; height: 490px;"><div class="vbox wb_container" id="wb_header" style="height: 150px; background: transparent none repeat scroll left top;">
 	
-<div id="wb_element_instance135" class="wb_element" style="left: 0px; top: 100px; width: 1000px; height: 40px; min-width: 1000px; min-height: 40px; z-index: 344;"><ul class="hmenu"><li><a href="Home/" target="_self" title="Home">Home</a></li><li><a href="Exercises/" target="_self" title="Exercises">Exercises</a></li><li><a href="Diet/" target="_self" title="Diet">Diet</a></li><li><a href="Supplements/" target="_self" title="Supplements">Supplements</a></li></ul></div><div id="wb_element_instance136" class="wb_element" style="left: 0px; top: 90px; width: 1000px; height: 60px; min-width: 1000px; min-height: 60px; z-index: 296;"><div style="background: #db0335; border: 0px solid #C0C0C0; opacity: 1; border-radius: 4px 4px 4px 4px; -moz-border-radius: 4px 4px 4px 4px; -webkit-border-radius: 4px 4px 4px 4px; width: 1000px; height: 60px;"></div></div><div id="wb_element_instance137" class="wb_element" style="left: 0px; top: 23px; width: 530px; height: 40px; min-width: 530px; min-height: 40px; z-index: 340;  line-height: normal;"><h4 class="wb-stl-pagetitle" style="text-align: left;">I want to get fit</h4>
+<div id="wb_element_instance135" class="wb_element" style="left: 0px; top: 100px; width: 1000px; height: 40px; min-width: 1000px; min-height: 40px; z-index: 374;"><ul class="hmenu"><li><a href="Home/" target="_self" title="Home">Home</a></li><li><a href="Exercises/" target="_self" title="Exercises">Exercises</a></li><li><a href="Diet/" target="_self" title="Diet">Diet</a></li><li><a href="Supplements/" target="_self" title="Supplements">Supplements</a></li></ul></div><div id="wb_element_instance136" class="wb_element" style="left: 0px; top: 90px; width: 1000px; height: 60px; min-width: 1000px; min-height: 60px; z-index: 326;"><div style="background: #db0335; border: 0px solid #C0C0C0; opacity: 1; border-radius: 4px 4px 4px 4px; -moz-border-radius: 4px 4px 4px 4px; -webkit-border-radius: 4px 4px 4px 4px; width: 1000px; height: 60px;"></div></div><div id="wb_element_instance137" class="wb_element" style="left: 0px; top: 23px; width: 530px; height: 40px; min-width: 530px; min-height: 40px; z-index: 370;  line-height: normal;"><h4 class="wb-stl-pagetitle" style="text-align: left;">I want to get fit</h4>
 </div></div>
-<div class="vbox wb_container" id="wb_main" style="height: 310px; background: transparent none repeat-x scroll left top; padding: 0 0 50px 0;">
+<div class="vbox wb_container" id="wb_main" style="height: 210px; background: transparent none repeat-x scroll left top; padding: 0 0 50px 0;">
 	
-<div id="wb_element_instance138" class="wb_element" style="left: 0px; top: 0px; width: 1000px; height: 310px; min-width: 1000px; min-height: 310px; z-index: 251; overflow: hidden;"><table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC"><tr><form name="form1" method="post" action="10.php">
+<div id="wb_element_instance138" class="wb_element" style="left: 0px; top: 0px; width: 1000px; height: 210px; min-width: 1000px; min-height: 210px; z-index: 257; overflow: hidden;"><table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC"><tr><form name="form1" method="post" action="10.php">
 <td>
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF"><tr><td colspan="3"><strong>Login</strong></td>
 </tr><tr><td width="75">Username</td>
@@ -88,9 +92,10 @@
 </tr><tr><td> </td>
 <td> </td>
 <td><input type="submit" name="Submit" value="Login"></td>
+
 </tr></table></td>
 </form>
-</tr></table></div><div id="wb_element_instance139" class="wb_element" style="left: 0px; top: 330px; min-width: 0px; min-height: 0px; z-index: 9000; width: 100%;">
+</tr></table></div><div id="wb_element_instance139" class="wb_element" style="left: 430px; top: 160px; width: 150px; height: 30px; min-width: 150px; min-height: 30px; z-index: 262;"><a class="wb_button" href="forgotten/"><span>Forgot your password?</span></a></div><div id="wb_element_instance140" class="wb_element" style="left: 0px; top: 230px; min-width: 0px; min-height: 0px; z-index: 9000; width: 100%;">
 			<?php
 				global $show_comments;
 				if (isset($show_comments) && $show_comments) {
@@ -98,7 +103,7 @@
 			?>
 			<script type="text/javascript">
 				$(function() {
-					var block = $("#wb_element_instance139");
+					var block = $("#wb_element_instance140");
 					var comments = block.children(".wb_comments").eq(0);
 					var contentBlock = $("#wb_main");
 					contentBlock.height(contentBlock.height() + comments.height());
@@ -109,7 +114,7 @@
 			?>
 			<script type="text/javascript">
 				$(function() {
-					$("#wb_element_instance139").hide();
+					$("#wb_element_instance140").hide();
 				});
 			</script>
 			<?php
@@ -118,7 +123,7 @@
 			</div></div>
 <div class="vbox wb_container" id="wb_footer" style="height: 80px; background: transparent none repeat-x scroll left top;">
 	
-<div id="wb_element_instance140" class="wb_element" style="left: 0px; top: 20px; min-width: 0px; min-height: 0px; z-index: 9999; text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+<div id="wb_element_instance141" class="wb_element" style="left: 0px; top: 20px; min-width: 0px; min-height: 0px; z-index: 9999; text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
 			$(function() {
 				var footer = $(".wb_footer");
 				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
@@ -128,5 +133,5 @@
 					footer.height(80);
 				}
 			});
-			</script></div></div><div class="wb_sbg" style="min-height: 590px;"></div></div></body>
+			</script></div></div><div class="wb_sbg" style="min-height: 490px;"></div></div></body>
 </html>
